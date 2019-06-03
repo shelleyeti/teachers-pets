d7b198a631be7b27f3dfdb60f795c024ec4e8e95
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-06-03 08:59:08 -0500

    prep for shelley merge

--- src/components/events/eventItem.js
+++ src/components/events/eventItem.js
@@ -32,24 +32,24 @@ class EventItem extends Component {
     });
   };
 
-  render() {
-      return (
-        <Card className="EventItem m-2">
-          {/* <CardImg top width="100%" src="" alt="Card image cap" /> */}
-          <CardBody>
-            <CardTitle>Title: {this.props.event.title}</CardTitle>
-            <CardText>Summary: {this.props.event.summary}</CardText>
-            <CardText>Location: {this.props.event.location}</CardText>
-            <CardText>Date: {this.props.event.date}</CardText>
-            <ButtonGroup className="btn-group ml-1" size="sm">
-              <Button onClick={() => {
-                  this.props.history.push(`/events/${this.props.event.id}/edit`);
-                }} className="btn btn-outline-primary eventBtn"><FaEdit /></Button>
-              <Button onClick={this.handleClickDelete} className="btn btn-outline-danger eventBtn"><FaTrashAlt /></Button>
-            </ButtonGroup>
-          </CardBody>
-        </Card>
-      );
+  render () {
+    return (
+      <Card className="EventItem m-2">
+        <CardImg top width="100%" src={ this.props.event.imgUrl } />
+        <CardBody>
+          <CardTitle>Title: { this.props.event.title }</CardTitle>
+          <CardText>Summary: { this.props.event.summary }</CardText>
+          <CardText>Location: { this.props.event.location }</CardText>
+          <CardText>Date: { this.props.event.date }</CardText>
+          <ButtonGroup className="btn-group ml-1" size="sm">
+            <Button onClick={ () => {
+              this.props.history.push(`/events/${this.props.event.id}/edit`);
+            } } className="btn btn-outline-primary eventBtn"><FaEdit /></Button>
+            <Button onClick={ this.handleClickDelete } className="btn btn-outline-danger eventBtn"><FaTrashAlt /></Button>
+          </ButtonGroup>
+        </CardBody>
+      </Card>
+    );
   }
 }
 
--- src/components/events/eventsList.js
+++ src/components/events/eventsList.js
@@ -4,36 +4,39 @@ import { Button, CardColumns } from "reactstrap";
 import EventItem from "./eventItem";
 
 class EventList extends Component {
-  render() {
+  render () {
     return (
-      <section className="Events">
-        <div className="addEventBtnDiv">
-          <Button
-            type="button"
-            className="btn btn-outline-primary m-3"
-            onClick={() => {
-              this.props.history.push("/events/new");
-            }}
-          >
-            Add Event
+      <section className="mainEventsContainer">
+
+        <section className="Events">
+          <div className="addEventBtnDiv">
+            <Button
+              type="button"
+              className="btn btn-outline-primary m-3"
+              onClick={ () => {
+                this.props.history.push("/events/new");
+              } }
+            >
+              Add Event
           </Button>
-        </div>
-        <h1 className="mx-5">Your Events</h1>
-        <CardColumns>
-          {this.props.events.map(item => {
-            if (item.userName === this.props.activeUser.userName) {
-            return (
-              <EventItem
-                activeUser={this.props.activeUser}
-                key={item.id}
-                event={item}
-                {...this.props}
-                deleteEvents={this.props.deleteEvents}
-              />
-            );
-            }
-          })}
-        </CardColumns>
+          </div>
+          <h1 className="mx-5">Your Events</h1>
+          <CardColumns>
+            { this.props.events.map(item => {
+              if (item.userName === this.props.activeUser.userName) {
+                return (
+                  <EventItem
+                    activeUser={ this.props.activeUser }
+                    key={ item.id }
+                    event={ item }
+                    { ...this.props }
+                    deleteEvents={ this.props.deleteEvents }
+                  />
+                );
+              }
+            }) }
+          </CardColumns>
+        </section>
       </section>
     );
   }
